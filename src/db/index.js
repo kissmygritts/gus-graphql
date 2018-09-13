dcae95451f80f4afa05255d351fa72435789f064
kissmygritts <kissmygritts08@gmail.com>
2018-09-13 16:33:19 -0700

    Add pg-monitor, ad hoc, not production ready

--- src/db/index.js
+++ src/db/index.js
@@ -1,6 +1,7 @@
 import promise from 'bluebird'
 const initOptions = { promiseLib: promise }
 const pgp = require('pg-promise')(initOptions)
+const monitor = require('pg-monitor')
 
 const config = {
   host: process.env.DB_HOST,
@@ -8,5 +9,8 @@ const config = {
   user: process.env.DB_USER,
   database: process.env.DB_NAME
 }
+
+monitor.setTheme('matrix')
+monitor.attach(initOptions)
 
 export const db = pgp(config)

73552afc1731720e2e681b35782be3a09094acf5
kissmygritts <kissmygritts08@gmail.com>
2018-09-17 11:52:32 -0700

    Formatting

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -1,7 +1,9 @@
 import { fileLoader, mergeTypes, mergeResolvers } from 'merge-graphql-schemas'
 import path from 'path'
 
-const typesArray = fileLoader(path.join(__dirname, './**/*.schema.graphql'), { recursive: true })
+const typesArray = fileLoader(path.join(__dirname, './**/*.schema.graphql'), {
+  recursive: true
+})
 const resolversArray = fileLoader(path.join(__dirname, './**/*.resolver.js'), { recursive: true })
 
 export const typeDefs = mergeTypes(typesArray)

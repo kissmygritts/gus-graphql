b933235fde1fc50da90df0526c67bed186104498
kissmygritts <kissmygritts08@gmail.com>
2018-09-17 12:18:37 -0700

    Pre-commit hook to prettier --write files

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -4,7 +4,9 @@ import path from 'path'
 const typesArray = fileLoader(path.join(__dirname, './**/*.schema.graphql'), {
   recursive: true
 })
-const resolversArray = fileLoader(path.join(__dirname, './**/*.resolver.js'), { recursive: true })
+const resolversArray = fileLoader(path.join(__dirname, './**/*.resolver.js'), {
+  recursive: true
+})
 
 export const typeDefs = mergeTypes(typesArray)
 export const resolvers = mergeResolvers(resolversArray)
